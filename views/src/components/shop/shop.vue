e5563c6d196f7f7840dd75107820e1880db85555
zzxprint <zzx971215@163.com>
2019-08-15 16:33:24 +0800

    商品侧边栏

--- views/src/components/shop/shop.vue
+++ views/src/components/shop/shop.vue
@@ -1,34 +1,22 @@
 <template>
     <div class="shop">
-        <van-tree-select
-        :items="items"
-        :main-active-index="mainActiveIndex"
-        :active-id="activeId"
-        @click-nav="onClickNav"
-        @click-item="onClickItem"
-        />
+        <side-list></side-list>
     </div>
 </template>
 
 <script>
+import sideList from './sideList'
 export default {
+    components: {
+        "side-list": sideList
+    },
     data() {
         return {
-            items: [],
-            mainActiveIndex: 0,
-			activeId: 1,
         };
 	},
 	created() {
-		this.items = this.$util.shopList()
 	},
     methods: {
-        onClickNav (index) {
-            this.mainActiveIndex = index
-        },
-        onClickItem (data) {
-            this.activeId = data.id
-        }
     }
 }
 </script>
---
+++ views/src/components/shop/sideList.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="side-list">
+        <ul>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+            <li>酒水</li>
+            <li>饮料</li>
+            <li>零食</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.side-list{
+    position: fixed;
+    top: 0;
+    bottom: 100px;
+    left: 0;
+    width: 160px;
+    border-right: 1px solid #efefef;
+    overflow: hidden;
+    ul{
+        z-index: 90;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: -30px;
+        padding: 20px 30px 20px 0;
+        background: #fefefe;
+        overflow-y: auto;
+        li{
+            font-size: 28px;
+            line-height: 90px;
+            text-align: center;
+        }
+    }
+}
+</style>
