b4d0016f53aa88a40551f3175f4b5560cd981081
zzxprint <zzxprint@163.com>
2019-08-26 22:31:13 +0800

    连接数据库

--- server/models/Category.js
+++ server/models/Category.js
@@ -3,6 +3,6 @@ var CategorySchema = require('../schemas/CategorySchema');
 
 //创建model，这个地方的product对应mongodb数据库中products的conllection。
 //mongoose会自动改成复数，如模型名：xx―>xxes, kitten―>kittens, money还是money
-var Category = mongoose.model('', CategorySchema);
+var Category = mongoose.model('category', CategorySchema);
 
 module.exports = Category;
