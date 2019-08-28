f82cd4748130855dd0753fad67a6bfbc3cec5476
zzxprint <zzx971215@163.com>
2019-08-28 10:01:10 +0800

    首页编写

--- views/src/components/shop/section/shopIcon.vue
+++ views/src/components/shop/section/shopIcon.vue
@@ -3,14 +3,38 @@
         <van-row type="flex" justify="space-around">
             <van-col span="5">
                 <svg-icon icon-class="noodles"></svg-icon>
-                <span>美味泡面</span>
+                <span>泡面</span>
             </van-col>
             <van-col span="5">
                 <svg-icon icon-class="chips"></svg-icon>
-                <span>休闲薯片</span>
+                <span>薯片</span>
             </van-col>
-            <van-col span="5"><svg-icon icon-class="drinks"></svg-icon></van-col>
-            <van-col span="5"></van-col>
+            <van-col span="5">
+                <svg-icon icon-class="drinks"></svg-icon>
+                <span>饮料</span>
+            </van-col>
+            <van-col span="5">
+                <svg-icon icon-class="chocolate"></svg-icon>
+                <span>巧克力</span>
+            </van-col>
+        </van-row>
+        <van-row type="flex" justify="space-around">
+            <van-col span="5">
+                <svg-icon icon-class="milk"></svg-icon>
+                <span>奶品</span>
+            </van-col>
+            <van-col span="5">
+                <svg-icon icon-class="bread"></svg-icon>
+                <span>面包</span>
+            </van-col>
+            <van-col span="5">
+                <svg-icon icon-class="biscuit"></svg-icon>
+                <span>饼干</span>
+            </van-col>
+            <van-col span="5">
+                <svg-icon icon-class="more"></svg-icon>
+                <span>更多</span>
+            </van-col>
         </van-row>
     </div>
 </template>
@@ -22,21 +46,22 @@ export default {
 
 <style lang="less" scoped>
 .shop-icon{
-    height: 300px;
+    padding: 10px 0 30px 0;
+    background: #FFF;
     .van-row{
-        padding-top: 20px;
+        padding-top: 30px;
         .van-col{
             text-align: center;
             .svg-icon{
-                height: 100px;
-                width: 100px;
+                height: 90px;
+                width: 90px;
             }
             span{
                 display: block;
-                font-size: 30px;
+                font-size: 26px;
                 text-align: center;
                 margin-top: 15px;
-                color: #666;
+                color: #777;
             }
         }
     }
--- views/src/components/shop/shop.vue
+++ views/src/components/shop/shop.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="shop">
         <shop-carousel></shop-carousel>
+        <shop-search></shop-search>
         <shop-icon></shop-icon>
     </div>
 </template>
 
 <script>
 import shopCarousel from './section/shopCarousel'
+import shopSearch from './section/shopSearch'
 import shopIcon from './section/shopIcon'
 export default {
     components: {
         'shop-carousel': shopCarousel,
+        'shop-search': shopSearch,
         'shop-icon': shopIcon
     }
 }
