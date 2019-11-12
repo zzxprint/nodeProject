3b54d87ba245032a0a326063695b5bd18282700f
zzxprint <zzx971215@163.com>
2019-11-12 17:15:21 +0800

    删除后管

---
+++ backstage/src/components/common/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.header{
+    height: 80px;
+    background: #000;
+}
+</style>
