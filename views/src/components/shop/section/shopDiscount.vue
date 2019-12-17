9bae84181e6305da777800f768db6e5c9744a54a
zzxprint <zzx971215@163.com>
2019-12-17 15:59:39 +0800

    删除多余生成小球代码

--- views/src/components/shop/section/shopDiscount.vue
+++ views/src/components/shop/section/shopDiscount.vue
@@ -73,38 +73,6 @@ export default {
             // 生成一个球
             this.$setDropBalloon()
         },
-        // 生成一个球
-        setBalloon() {
-            // 获取坐标
-            let x = event.clientX
-            let y = window.screen.availHeight - event.clientY
-            // 获取父元素
-            let parentDiv = document.querySelector('#app')
-            // 生成子元素
-            let childDiv = document.createElement('div')
-            childDiv.className = 'drop-balloon'
-            childDiv.style.left = (x - 10) + 'px'
-            childDiv.style.bottom = (y - 13) + 'px'
-            // 生成vant元素
-            let vantDiv = document.createElement('i')
-            vantDiv.className = 'van-icon van-icon-cart-circle cart-icon'
-            childDiv.appendChild(vantDiv)
-            // 动画
-            setTimeout(() => {
-                childDiv.className += ' ' + 'left'
-            }, 10);
-            setTimeout(() => {
-                childDiv.className += ' ' + 'drop'
-                vantDiv.className += ' ' + 'shrink'
-            }, 30);
-            // 放到父元素中
-            parentDiv.appendChild(childDiv)
-            // 动画执行完删除元素
-            setTimeout(() => {
-                let ball = document.getElementsByClassName('drop-balloon')
-                ball[0].remove()
-            }, 1000)
-        },
     }
 }
 </script>
@@ -169,35 +137,5 @@ export default {
             }
         }
     }
-}
-</style>
-
-<style>
-.drop-balloon{
-    position: fixed;
-    height: 60px;
-    width: 60px;
-    z-index: 999;
-    border-radius: 50%;
-    /* background: #00AAEE; */
-    transition: all 1s;
-}
-.left{
-    left: 440px !important;
-    transition: all linear 0.8s;
-}
-.drop{
-    bottom: 0px !important;
-    height: 0;
-    width: 0;
-    transition: cubic-bezier(.5,-0.25,.9,-0.09) 0.7s;
-}
-.cart-icon{
-    font-size: 60px;
-    color: #00AAEE;
-}
-.shrink{
-    font-size: 1px !important;
-    transition: all 1s;
 }
 </style>
